abc5485d03a39b3fbfed27624a76e153da5a9434
Imal16 <malek.ihsaan@gmail.com>
2021-04-09 20:30:16 -0400

    Redid Entire Website from Scratch
    Recoded the entire website from scratch to make it responsive, used bootstrap and slick, with a little js

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+
+$(document).ready(function(){
+    $(".carousel").slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplayspeed: 4000,
+        slidesToShow: 3,
+        slidesToScroll: 1,        
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              arrows:true
+            }
+          },
+          {
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              swipe: true,
+              arrows: false
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              swipe: true,
+              arrows: false
+            },
+          },
+
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+});
